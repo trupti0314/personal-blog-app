90e65accff525fcfe63fb78dc3ff45aa4f78f44c
trupti0314 <truptikulkarni0314@gmail.com>
2025-01-03 12:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def upload_file():
     return "Invalid file format or no file selected."
  
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=80)
